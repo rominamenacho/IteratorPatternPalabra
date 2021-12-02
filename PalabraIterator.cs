ae4c05d488a996b3fddae28b1501b8cea9c8a8ac
rominamenacho <43242020+rominamenacho@users.noreply.github.com>
2021-12-02 14:45:50 -0300

    .

---
+++ PalabraIterator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+
+namespace IteratorPattern_Native
+{
+    public class PalabraIterator: IEnumerator
+    {
+        private char [] palabra;
+        int _pos;
+
+        public PalabraIterator(string palabra)
+        {
+            this.palabra = palabra.ToCharArray();
+            _pos = -1;
+        }
+
+        public object Current => palabra[_pos];
+
+        public bool MoveNext()
+        {
+           if(_pos < palabra.Length - 1)
+            {
+                _pos++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            _pos = -1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorPattern_Native
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* Palabra p = new Palabra("hola mundo");
+
+            foreach(var letra in p)
+            {
+                Console.WriteLine(letra);
+            }
+            */
+
+            foreach (var letra in new Palabra("hola juan carlos"))
+            {
+                Console.WriteLine(letra);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
